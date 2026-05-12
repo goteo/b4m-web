45e0253ba29c32b5e7aec729db31f26c2a69aa57
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-05-12 15:26:57 +0200

    feat: add translations

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -1,8 +1,10 @@
 import ca from "./ca.json";
+import el from "./el.json";
 import en from "./en.json";
 import es from "./es.json";
+import it from "./it.json";
 
-export const labels = { es, en, ca };
+export const labels = { es, en, ca, it, el };
 export type Locale = keyof typeof labels;
 
 export function isSupportedLocale(code: string): code is Locale {
@@ -10,7 +12,9 @@ export function isSupportedLocale(code: string): code is Locale {
 }
 
 export const languagesList: Record<Locale, string> = {
-    es: "Español",
     en: "English",
+    es: "Español",
+    it: "Italiano",
+    el: "ελληνική",
     ca: "Català",
 };
