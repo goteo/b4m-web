deef7cc0361bdab5e0e6327eedac95aa87477325
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-05-14 17:30:05 +0200

    fix: bad translations

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -3,11 +3,10 @@ import ca from "./ca.json";
 import el from "./el.json";
 import en from "./en.json";
 import es from "./es.json";
-import fr from "./fr.json";
 import hr from "./hr.json";
 import it from "./it.json";
 
-export const labels = { es, en, ca, it, el, hr, bs, fr };
+export const labels = { es, en, ca, it, el, hr, bs };
 export type Locale = keyof typeof labels;
 
 export function isSupportedLocale(code: string): code is Locale {
@@ -22,5 +21,4 @@ export const languagesList: Record<Locale, string> = {
     ca: "Català",
     hr: "Hrvatski",
     bs: "Bosanski",
-    fr: "Français",
 };
