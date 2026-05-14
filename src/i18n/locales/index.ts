2316851715a89ad9ae727da7cf97f99c5e4c7bb6
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-05-14 12:27:27 +0200

    feat: add missing languages hr, bs and fr

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -1,10 +1,13 @@
+import bs from "./bs.json";
 import ca from "./ca.json";
 import el from "./el.json";
 import en from "./en.json";
 import es from "./es.json";
+import fr from "./fr.json";
+import hr from "./hr.json";
 import it from "./it.json";
 
-export const labels = { es, en, ca, it, el };
+export const labels = { es, en, ca, it, el, hr, bs, fr };
 export type Locale = keyof typeof labels;
 
 export function isSupportedLocale(code: string): code is Locale {
@@ -17,4 +20,7 @@ export const languagesList: Record<Locale, string> = {
     it: "Italiano",
     el: "ελληνική",
     ca: "Català",
+    hr: "Hrvatski",
+    bs: "Bosanski",
+    fr: "Français",
 };
